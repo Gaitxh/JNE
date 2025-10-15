bf783b831fef238e83e3dced6edd6c06d7d77df1
Gao Xiaohui (高晓辉) <99315754+Gaitxh@users.noreply.github.com>
2025-10-15 14:54:23 +0800

    Update T3_IndicesRepresente.py

--- T3_IndicesRepresente.py
+++ T3_IndicesRepresente.py
@@ -28,9 +28,15 @@ sub_set = [1]
 device = torch.device("cpu")
 project_dir = os.path.dirname(os.path.abspath(__file__))
 for sub_idx in sub_set:
-    for run_i in ['clip_vit', 'vit']:
+    for run_i in [
+                  'clip_vit'
+                  #'vit'
+                 ]:
         for layer_i in range(1, 13):  # 注意，第一层是embedding的输出，后面是vit模块12层的输出
-            for encoder_type in ['res_linear', 'res_nonlinear_relu', 'res_nonlinear_gelu']:
+            for encoder_type in ['res_linear', 
+                                 'res_nonlinear_relu' 
+                                 # 'res_nonlinear_gelu'
+                                ]:
                 for bias in [False]:
                     for hidden_size in [1024]:
                         "加载模型权重"
@@ -55,3 +61,4 @@ for sub_idx in sub_set:
                               f'R2max{R2_.max():.2f}-mean{np.mean(R2_):.4f}, '
                               f'Sig voxel{len(R2_)}'
                               )
+
