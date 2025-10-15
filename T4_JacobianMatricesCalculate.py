6fc6ce225213924eec6e9acf514341e70a017279
Gao Xiaohui (高晓辉) <99315754+Gaitxh@users.noreply.github.com>
2025-10-15 14:55:01 +0800

    Update T4_JacobianMatricesCalculate.py

--- T4_JacobianMatricesCalculate.py
+++ T4_JacobianMatricesCalculate.py
@@ -27,9 +27,8 @@ sub_set = [1]
 device = torch.device("cpu")
 project_dir = os.path.dirname(os.path.abspath(__file__))
 for sub_idx in sub_set:
-    for run_i in ['clip_vit', 'vit']:
+    for run_i in ['clip_vit']:
         for layer_i in range(1, 13):  # 注意，第一层是embedding的输出，后面是vit模块12层的输出
-            "鉴于'res_linear', 'res_nonlinear_relu', 'res_nonlinear_gelu'三者相比，'res_nonlinear_relu'比'res_nonlinear_gelu'更好的结果，因此选用relu作为激活函数分析"
             for encoder_type in ['res_linear', 'res_nonlinear_relu']:
                 for bias in [False]:
                     for hidden_size in [1024]:
@@ -116,3 +115,4 @@ for sub_idx in sub_set:
                                       f'{encoder_type}, '
                                       f'has wrong. '
                                       )
+
